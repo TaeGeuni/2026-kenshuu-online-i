f455ec6debbf33ca6b48d8f1b94c4dedc311b3b4
Taegeuni <qoxorms1504@gmail.com>
2026-04-17 13:44:19 +0900

    feat: solve application problems 1 & 2

---
+++ python/02_応用編/2. 変数 (応用)/Question2_1 応用.py
@@ -0,0 +1,13 @@
+# [KR] 각각의 데이터 타입에 맞는 값을 임의의 변수명에 대입합니다.
+# [JP] それぞれのデータ型に合った値を任意の変数名に代入します。
+num_int = 12  # 정수 (整数 - int)
+num_float = 1.6  # 부동소수점 (浮動小数点数 - float)
+greeting_str = "こんにちは"  # 문자열 (文字列 - str)
+is_active = True  # 불리언 (ブール値 - bool)
+
+# [KR] 각 변수에 저장된 값을 순서대로 출력합니다.
+# [JP] 各変数に格納された値を順番に出力します。
+print(num_int)
+print(num_float)
+print(greeting_str)
+print(is_active)
